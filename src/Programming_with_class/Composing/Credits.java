7759cc928ff310b182d2c5250cfe6dbcd34350f4
Redian23 <vasya.degski@gmail.com>
2021-02-16 23:41:35 +0300

    update class Client

--- src/Programming_with_class/Composing/Credits.java
+++ src/Programming_with_class/Composing/Credits.java
@@ -1,53 +1,126 @@
 package Programming_with_class.Composing;
+import static Programming_with_class.Composing.ClientAccount.*;
 
-class Account{
+class Account {
+    String accountName;
     String bankName;
     int accountNumber;
-    final int money;
-    private boolean accountStatus = true;
+    int money;
+    boolean accountStatus;
 
-    Account (String bankName,int money, int accountNumber){
+    Account(String accountName, String bankName, int money, int accountNumber, boolean accountStatus) {
+        this.accountName = accountName;
         this.bankName = bankName;
         this.accountNumber = accountNumber;
         this.money = money;
+        this.accountStatus = accountStatus;
     }
-    void accountStatus(){
-        if(accountStatus == false) {
-            System.out.println("Account is lock");
-        }else
-            System.out.println("Account working");
-    }
-    void lockAccount(){
+
+    public void lockAccount() {
         this.accountStatus = false;
-        System.out.println("Account was been lock");
     }
 
-    void unlockAccount(){
+    public void unlockAccount() {
         this.accountStatus = true;
-        System.out.println("Account was been unlock");
-    }
-
-    void getMoneyOfAccount(){
-        if(money<0){
-            System.out.println("Account have a negative money value");
-            System.out.println("You are dept to bank " + money);
-        }
     }
 }
 
-class Client {
-    Account account1 = new Account("HomeBank",200,177013);
-    Account account2 = new Account("HomeBank",-415,152417);
+class ClientAccount {
+    static Account[] accountsClient1 = new Account[4];
+    private boolean accountStatus;
+    private Client client;
+
+    void accountStatus(Account[] accountsClient1) {
+        for (Account account : accountsClient1) {
+            if (account.accountStatus == false) {
+                System.out.println(account.accountName + " is lock");
+            } else
+                System.out.println(account.accountName + "  working");
+        }
+    }
+
+    void lockAccount(Account accountsClient1) {
+        accountsClient1.lockAccount();
+        System.out.println(accountsClient1.accountName + " was been lock");
+    }
+
+    void unlockAccount(Account accountsClient1) {
+        accountsClient1.unlockAccount();
+        System.out.println(accountsClient1.accountName + " was been unlock");
+    }
+
+    void getMoneyOfAccount(Account[] accountsClient1) {
+        for (Account account : accountsClient1) {
+            if (account.money < 0) {
+                System.out.println("You are dept to bank " + account.money + " $ of " + account.accountName);
+            } else
+                System.out.println("You are have a - " + account.money + " $ of " + account.accountName);
+        }
+    }
+
+    void getSumAllAccounts() {
+        int postSum = 0;
+        int negaSum = 0;
+        for (Account account : accountsClient1) {
+            if (account.money < 0) {
+                negaSum += account.money;
+            } else {
+                postSum += account.money;
+            }
+        }
+        System.out.println("Positive money sum value " + postSum + " $");
+        System.out.println("and");
+        System.out.println("Negative money sum value " + negaSum + " $");
+    }
+
+    void searchNeedAccount(String accountName){
+        for (Account account: accountsClient1) {
+            if (account.accountName.equals(accountName)){
+                System.out.println( "Account Name - " + account.accountName +
+                        "\n Bank - " + account.bankName +
+                        "\n Money - "+ account.money +" $"+
+                        "\n Number - " + account.accountNumber +
+                        "\n Status - " + account.accountStatus);
+            }
+        }
+    }
+
+    void sortAccounts(){
+
+
+        for (Account account : accountsClient1) {
+            System.out.println(account.accountName + " " +
+                    account.bankName + " " +
+                    account.money + " " +
+                    account.accountNumber + " " +
+                    account.accountStatus);
+        }
+
+    }
+}
+class Client{
+    ClientAccount cl = new ClientAccount();
 
     public static void main(String[] args) {
     Client client1 = new Client();
-    client1.account1.accountStatus();
-    client1.account1.lockAccount();
-    client1.account1.accountStatus();
-    client1.account1.unlockAccount();
-    client1.account1.accountStatus();
 
-    client1.account1.getMoneyOfAccount();
+    accountsClient1[0]= new Account("Card1","HomeBank",200,177013,true);
+    accountsClient1[1]= new Account("Card2","HomeBank",-355,152417,true);
+    accountsClient1[2]= new Account("Card3","HomeBank",-155,102499,true);
+    accountsClient1[3]= new Account("Card4","SberBank",285,120547,true);
+
+    client1.cl.getSumAllAccounts();
+        System.out.println(" ");
+    client1.cl.accountStatus(accountsClient1);
+        System.out.println(" ");
+    client1.cl.lockAccount(accountsClient1[1]);
+        System.out.println(" ");
+    client1.cl.getMoneyOfAccount(accountsClient1);
+        System.out.println(" ");
+    client1.cl.searchNeedAccount("Card2");
+        System.out.println(" ");
+    client1.cl.sortAccounts();
+
     }
 
 
